470ccc1dcfda01cdaab0ff3c5f680d3345886c7b
tomiho19 <sandrez.spartanell@gmail.com>
2020-03-24 19:08:41 +0200

    initial tests for contractor component

--- src/app/pages/ecommerce/sellers/sellers.component.spec.ts
+++ src/app/pages/ecommerce/sellers/sellers.component.spec.ts
@@ -1,25 +1,47 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { SellersComponent } from './sellers.component';
+import {SellersComponent} from './sellers.component';
+import {DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser';
 
 describe('SellersComponent', () => {
-  let component: SellersComponent;
-  let fixture: ComponentFixture<SellersComponent>;
+    let component: SellersComponent;
+    let fixture: ComponentFixture<SellersComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SellersComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [SellersComponent]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SellersComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SellersComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should generate create form controls', () => {
+        expect(component.cf).toBeTruthy();
+    });
+
+    it('should generate update form controls', () => {
+        expect(component.uf).toBeTruthy();
+    });
+
+    it('should open edit user modal', () => {
+        const openEl: DebugElement = fixture.debugElement.query(By.css('#openEditModal'));
+        openEl.nativeElement.click();
+        expect(component.openModal).toHaveBeenCalled();
+    });
+
+    it('should call delete handler', () => {
+        const closeEl: DebugElement = fixture.debugElement.query(By.css('#deleteContractor'));
+        closeEl.nativeElement.click();
+        expect(component.delete).toHaveBeenCalled();
+    });
 });
