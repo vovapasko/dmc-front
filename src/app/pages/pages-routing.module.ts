c8c9f9da7d6bfd71cba558b4b79d96be5a47f2c4
tomiho19 <sandrez.spartanell@gmail.com>
2020-03-30 17:20:08 +0300

    routing counterparties -> contractors

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -5,7 +5,7 @@ export const routes: Routes = [
   {path: '', redirectTo: '/dashboards/dashboard', pathMatch: 'full'},
   { path: 'dashboard', loadChildren: () => import('./dashboards/dashboards.module').then(m => m.DashboardsModule) },
   { path: 'users', loadChildren: () => import('./crm/crm.module').then(m => m.CRMModule) },
-  { path: 'counterparties', loadChildren: () => import('./ecommerce/ecommerce.module').then(m => m.EcommerceModule) },
+  { path: 'contractors', loadChildren: () => import('./ecommerce/ecommerce.module').then(m => m.EcommerceModule) },
   { path: 'burst-news', loadChildren: () => import('./form/form.module').then(m => m.FormModule) },
   { path: 'image-crop', loadChildren: () => import('./form/form.module').then(m => m.FormModule) },
   { path: 'bursted-news', loadChildren: () => import('./apps/apps.module').then(m => m.AppsModule) },
