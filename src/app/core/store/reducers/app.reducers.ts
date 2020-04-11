7524bcc7223235b4969e68cc0008794ba08b94b8
tomiho19 <sandrez.spartanell@gmail.com>
2020-04-11 16:20:39 +0300

    ngrx correct providing reducers to app

--- src/app/core/store/reducers/app.reducers.ts
+++ src/app/core/store/reducers/app.reducers.ts
@@ -4,6 +4,9 @@ import {routerReducer} from '@ngrx/router-store';
 import {IAppState} from '../state/app.state';
 import {userReducers} from './user.reducers';
 import {contractorReducers} from './contractor.reducers';
+import {InjectionToken} from '@angular/core';
+
+export const reducerToken: InjectionToken<ActionReducerMap<IAppState>> = new InjectionToken<ActionReducerMap<IAppState>>('Reducers');
 
 export function appReducers(): ActionReducerMap<IAppState, any>  {
     return {
@@ -12,3 +15,7 @@ export function appReducers(): ActionReducerMap<IAppState, any>  {
         contractors: contractorReducers
     };
 }
+
+export const reducerProvider = { provide: reducerToken, useFactory: appReducers };
+
+
