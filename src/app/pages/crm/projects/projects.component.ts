23d1669ad9c9cfde89a5df4ee4d84d861fae9c95
tomiho19 <sandrez.spartanell@gmail.com>
2020-04-21 13:25:04 +0300

    provide loading for users and contractors

--- src/app/pages/crm/projects/projects.component.ts
+++ src/app/pages/crm/projects/projects.component.ts
@@ -2,7 +2,10 @@ import {Component, OnInit} from '@angular/core';
 import {select, Store} from '@ngrx/store';
 import {IAppState} from '../../../core/store/state/app.state';
 import {selectContractors, selectProjects} from '../../../core/store/selectors/news.selectors';
-import {Router} from "@angular/router";
+import {Router} from '@angular/router';
+import {ErrorService} from '../../../core/services/error.service';
+import {LoadingService} from '../../../core/services/loading.service';
+import {Subject} from 'rxjs';
 
 @Component({
     selector: 'app-projects',
@@ -18,12 +21,16 @@ export class ProjectsComponent implements OnInit {
     // bread crumb items
     breadCrumbItems: Array<{}>;
     projects$ = this.store.pipe(select(selectProjects));
+    loading$: Subject<boolean>;
+    error$: Subject<any>;
 
     order = null;
 
     constructor(
         private store: Store<IAppState>,
         private router: Router,
+        private errorService: ErrorService,
+        private loadingService: LoadingService,
     ) {
     }
 
@@ -34,7 +41,14 @@ export class ProjectsComponent implements OnInit {
         /**
          * fetches data
          */
+        this.initSubscriptions();
         this._fetchData();
+    }
+
+    initSubscriptions() {
+        this.loading$ = this.loadingService.loading$;
+        this.error$ = this.errorService.error$;
+
     }
 
     onCreateProject() {
