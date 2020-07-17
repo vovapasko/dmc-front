b03e4b323928f4796c7ed4fc5a4a030d7490d064
sa1exx <sandrez.spartanell@gmail.com>
2020-07-17 09:05:21 +0300

    removed useless code

--- src/app/core/models/instances/email.ts
+++ src/app/core/models/instances/email.ts
@@ -6,9 +6,3 @@ export interface Email {
   template: string;
   signature: string;
 }
-//"id": 1,
-//             "date_created": "2020-05-25T10:05:55.423734Z",
-//             "date_updated": "2020-05-25T10:08:45.077604Z",
-//             "email": "mail@mail.com",
-//             "template": "my templat e",
-//             "signature": "my signatu re"
