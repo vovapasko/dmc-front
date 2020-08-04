82a7d4a19f4f3f02cef6e457e79fe02a9b5ee540
sa1exx <sandrez.spartanell@gmail.com>
2020-08-04 14:57:30 +0300

    tests for email

--- src/app/pages/email/inbox/inbox.component.spec.ts
+++ src/app/pages/email/inbox/inbox.component.spec.ts
@@ -1,6 +1,40 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InboxComponent } from './inbox.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import {
+  NgbAlertModule,
+  NgbDatepickerModule,
+  NgbDropdownModule,
+  NgbModalModule,
+  NgbPaginationModule, NgbPopoverModule, NgbProgressbarModule, NgbTooltipModule,
+  NgbTypeaheadModule
+} from '@ng-bootstrap/ng-bootstrap';
+import { ClickOutsideModule } from 'ng-click-outside';
+import { UIModule } from '../../../shared/ui/ui.module';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Store, StoreModule } from '@ngrx/store';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { QuillModule } from 'ngx-quill';
+import { NgxEditorModule } from 'ngx-editor';
+import { ColorPickerModule } from 'ngx-color-picker';
+import { UiSwitchModule } from 'ngx-ui-switch';
+import { FileUploadModule } from '@iplab/ngx-file-upload';
+import { ArchwizardModule } from 'angular-archwizard';
+import { NgxMaskModule } from 'ngx-mask';
+import { NestableModule } from 'ngx-nestable';
+import { SharedModule } from '../../../shared/shared.module';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { AuthenticationService } from '@services/auth.service';
+import { Title } from '@angular/platform-browser';
+import { ErrorService } from '@services/error.service';
+import { LoadingService } from '@services/loading.service';
+import { NotificationService } from '@services/notification.service';
+import { NewsService } from '@services/news.service';
 
 describe('InboxComponent', () => {
   let component: InboxComponent;
@@ -8,6 +42,50 @@ describe('InboxComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        RouterModule,
+        NgbDropdownModule,
+        ClickOutsideModule,
+        UIModule,
+        NgbAlertModule,
+        NgbModalModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+        StoreModule.forRoot({}),
+        FormsModule,
+        NgbPaginationModule,
+        NgApexchartsModule,
+        NgbTypeaheadModule,
+        Ng2SearchPipeModule,
+        NgSelectModule,
+        QuillModule.forRoot(),
+        NgxEditorModule,
+        NgbDropdownModule,
+        NgbDatepickerModule,
+        ColorPickerModule,
+        UiSwitchModule,
+        FileUploadModule,
+        ArchwizardModule,
+        NgxMaskModule.forRoot(),
+        NestableModule,
+        NgbPopoverModule,
+        SharedModule,
+        NgbProgressbarModule,
+        NgbTooltipModule
+      ],
+      providers: [
+        HttpClient,
+        HttpHandler,
+        FormBuilder,
+        AuthenticationService,
+        Title,
+        ErrorService,
+        LoadingService,
+        Store,
+        NotificationService,
+        NewsService
+      ],
       declarations: [ InboxComponent ]
     })
     .compileComponents();
