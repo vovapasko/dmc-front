55564c5e78b1ec5e5ed3cc7701c13ddb7866dbbf
sa1exx <sandrez.spartanell@gmail.com>
2020-09-21 14:13:32 +0300

    request handler

--- src/app/core/helpers/request-handler.ts
+++ src/app/core/helpers/request-handler.ts
@@ -7,13 +7,14 @@ import { ResponseHandler } from './response-handler';
 import { ErrorHandler } from './error-handler';
 import { ConvertCase } from './convert-case';
 import { Payloads } from '@models/payloads/payload';
+import { dataTitle } from '@constants/data';
 
 /**
  * This service handle request from clients, process and send to server
  */
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class RequestHandler {
   constructor(
@@ -21,30 +22,30 @@ export class RequestHandler {
     private responseHandler: ResponseHandler,
     private errorHandler: ErrorHandler,
     private convertCase: ConvertCase
-  ) {}
+  ) {
+  }
 
   processPayload(payload) {
     if (!payload) {
       return {};
     }
-    if (payload && 'data' in payload && payload.data instanceof FormData) {
+    if (payload && dataTitle in payload && payload.data instanceof FormData) {
       return payload.data;
     }
     return this.convertCase.convertFromCamelToSnakeCase(payload.data);
   }
 
-  request(url, method, payload?: Payloads, mapHandler = (res) => {}) {
-    return this.http[method](url, this.processPayload(payload)).pipe(
-      tap((response: ServerResponse) => this.responseHandler.handle(response)),
-      map(
-        (response: ServerResponse) => {
-          const convertedCaseResponse = this.convertCase.convertFromSnakeToCamelCase(response);
-          return mapHandler(convertedCaseResponse);
-        },
-        (error) => {
-          // console.log(error);
-        }
-      )
-    );
+  request(url, method, payload?: Payloads, mapHandler = (_) => {
+  }) {
+    return this.http[method](url, this.processPayload(payload))
+      .pipe(
+        tap((response: ServerResponse) => this.responseHandler.handle(response)),
+        map(
+          (response: ServerResponse) => {
+            const convertedCaseResponse = this.convertCase.convertFromSnakeToCamelCase(response);
+            return mapHandler(convertedCaseResponse);
+          }
+        )
+      );
   }
 }
