da8474098ef9e3e0337c210aff51b5b12c47ae4c
sa1exx <sandrez.spartanell@gmail.com>
2020-09-21 13:25:29 +0300

    radial bars

--- src/app/core/components/charts/data.ts
+++ src/app/core/components/charts/data.ts
@@ -1,88 +1,79 @@
 const multipleRadialBars = {
-    chart: {
-        height: 350,
-        type: 'radialBar',
-    },
-    plotOptions: {
-        radialBar: {
-            dataLabels: {
-                name: {
-                    fontSize: '22px',
-                },
-                value: {
-                    fontSize: '16px',
-                    formatter(val) {
-                        return val + '$';
-                    }
-                },
-                total: {
-                    show: false
-                }
-            }
+  chart: {
+    height: 350,
+    type: 'radialBar'
+  },
+  plotOptions: {
+    radialBar: {
+      dataLabels: {
+        name: {
+          fontSize: '22px'
+        },
+        value: {
+          fontSize: '16px',
+          formatter(val) {
+            return val + '$';
+          }
+        },
+        total: {
+          show: false
         }
-    },
-    colors: ['#56c2d6', '#e36498', '#23b397', '#23b397'],
-    series: [0, 0, 0, 0],
-    labels: ['Месяц', 'Неделю', 'День', 'Час'],
+      }
+    }
+  },
+  colors: ['#56c2d6', '#e36498', '#23b397', '#23b397'],
+  series: [0, 0, 0, 0],
+  labels: ['Месяц', 'Неделю', 'День', 'Час']
 };
-
-
-// Revenue table data
-export interface RevenueData {
-    marketplaces: string;
-    date: string;
-    payout: string;
-    status: string;
-}
 
 // Chart data
 export interface ChartType {
-    chart?: any;
-    plotOptions?: any;
-    colors?: any;
-    series?: any;
-    stroke?: any;
-    fill?: any;
-    labels?: any;
-    markers?: any;
-    legend?: any;
-    xaxis?: any;
-    yaxis?: any;
-    tooltip?: any;
-    grid?: any;
+  chart?: any;
+  plotOptions?: any;
+  colors?: any;
+  series?: any;
+  stroke?: any;
+  fill?: any;
+  labels?: any;
+  markers?: any;
+  legend?: any;
+  xaxis?: any;
+  yaxis?: any;
+  tooltip?: any;
+  grid?: any;
 }
 
 
 const revenueRadialChart: ChartType = {
-    chart: {
-        height: 200,
-        type: 'radialBar',
-    },
-    plotOptions: {
-        radialBar: {
-            hollow: {
-                size: '65%',
-            },
-            dataLabels: {
-                name: {
-                    show: false,
-                },
-                value: {
-                    fontSize: '24px',
-                    color: 'rgb(241, 85, 108)',
-                    offsetY: 10,
-                    formatter: (val) => {
-                        return val + '%';
-                    },
-                },
-            },
+  chart: {
+    height: 200,
+    type: 'radialBar'
+  },
+  plotOptions: {
+    radialBar: {
+      hollow: {
+        size: '65%'
+      },
+      dataLabels: {
+        name: {
+          show: false
         },
-    },
-    colors: ['rgb(241, 85, 108)'],
-    series: [0],
-    stroke: {
-        lineCap: 'round',
-    },
+        value: {
+          fontSize: '24px',
+          color: 'rgb(241, 85, 108)',
+          offsetY: 10,
+          formatter: (val) => {
+            return val + '%';
+          }
+        }
+      }
+    }
+  },
+  colors: ['rgb(241, 85, 108)'],
+  series: [0],
+  stroke: {
+    lineCap: 'round'
+  }
 };
 
-export {multipleRadialBars, revenueRadialChart};
+export { multipleRadialBars, revenueRadialChart };
