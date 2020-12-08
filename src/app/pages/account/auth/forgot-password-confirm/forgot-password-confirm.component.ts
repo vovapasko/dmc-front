fdba3c00f47de370a7563d6a56ac16dee7af65d5
sa1exx <sandrez.spartanell@gmail.com>
2020-12-08 10:34:02 +0200

    forgot password confirm component

---
+++ src/app/pages/account/auth/forgot-password-confirm/forgot-password-confirm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forgot-password-confirm',
+  templateUrl: './forgot-password-confirm.component.html',
+  styleUrls: ['./forgot-password-confirm.component.scss']
+})
+export class ForgotPasswordConfirmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
