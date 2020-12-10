6daa9f583d0a354cdd01940b805671d88268896d
sa1exx <sandrez.spartanell@gmail.com>
2020-12-10 09:10:33 +0200

    payload edit

--- src/app/core/models/payloads/user/delete.ts
+++ src/app/core/models/payloads/user/delete.ts
@@ -2,5 +2,7 @@ import { ArchiveEntity } from '@models/payloads/archive/archive-entity';
 
 export interface DeleteUserPayload {
   id: number | string;
-  data: ArchiveEntity;
+  data: {
+    isActive: boolean;
+  };
 }
