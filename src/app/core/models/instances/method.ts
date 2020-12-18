b7174536f1f7d386003bbadb90fdc5a632171932
sa1exx <sandrez.spartanell@gmail.com>
2020-12-18 13:33:31 +0200

    updated burst methods methods

--- src/app/core/models/instances/method.ts
+++ src/app/core/models/instances/method.ts
@@ -3,14 +3,14 @@ export interface Method {
   method: string;
 }
 
-export enum Methods {
-  topSecret = 'top secret',
-  bayer = 'bayer',
-  direct = 'direct'
-}
-
 // export enum Methods {
-//   topSecret = 'топ сикрет',
-//   bayer = 'секретно',
-//   direct = 'на прямую'
+//   topSecret = 'top secret',
+//   bayer = 'bayer',
+//   direct = 'direct'
 // }
+
+export enum Methods {
+  topSecret = 'топ сикрет',
+  bayer = 'секретно',
+  direct = 'на прямую'
+}
