0a002e9f920226d46d08df488dcfbb18b5d61939
sa1exx <sandrez.spartanell@gmail.com>
2020-12-18 13:33:02 +0200

    updated methods

--- src/app/core/models/instances/method.ts
+++ src/app/core/models/instances/method.ts
@@ -8,3 +8,9 @@ export enum Methods {
   bayer = 'bayer',
   direct = 'direct'
 }
+
+// export enum Methods {
+//   topSecret = 'топ сикрет',
+//   bayer = 'секретно',
+//   direct = 'на прямую'
+// }
